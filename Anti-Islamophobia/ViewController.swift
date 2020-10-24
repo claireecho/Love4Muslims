4abe51cec8659d970d5ba5ff7c35e7176fcd49de
Claire Cho <claire.eunbee@gmail.com>
2020-10-24 17:43:05 -0400

    Initial Commit

---
+++ Anti-Islamophobia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anti-Islamophobia
+//
+//  Created by Claire Cho on 10/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
