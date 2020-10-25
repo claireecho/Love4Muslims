0187d9aa9e268122b14fbcb7860fee081d8e84b0
Claire Cho <claire.eunbee@gmail.com>
2020-10-24 20:27:17 -0400

    Change name and removed original logo

--- Anti-Islamophobia/ViewController.swift
+++ Anti-Islamophobia/ViewController.swift
@@ -11,27 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.background.alpha = 0
-        self.logo.alpha = 0
-        UIView.animate(withDuration: 1) {
-            self.background.alpha = 1
-        } completion: { (Bool) in
-            UIView.animate(
-                withDuration: 1,
-                animations: {
-                    self.logo.alpha = 1
-            }, completion: { (Bool) in
-                sleep(1)
-                self.performSegue(withIdentifier: "toMain", sender: self)
-            })
-
-        }
 
     }
-    @IBOutlet weak var background: UIImageView!
-    @IBOutlet weak var logo: UIImageView!
-    @IBOutlet weak var background2: UIImageView!
-    
 
 }
 
